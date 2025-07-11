90a2baf4655f18522cf69f7be2444d4800e15d9c
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2025-07-11 01:03:59 -0400

    Add GitHub Actions deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aaronmolina.github.io',
+  base: '/aaronmolina-astro',
   vite: {
     plugins: [tailwindcss()],
     server: {

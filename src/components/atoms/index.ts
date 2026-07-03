1d68fa3b0a7d3b8152bf715b9261211761760545
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2026-07-03 11:54:34 -0400

    chore: remove Tag.astro, superseded by Badge.astro phase/stamp variants

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,5 @@
 // Atom Components - Basic building blocks
 export { default as Button } from './Button.astro';
-export { default as Tag } from './Tag.astro';
 export { default as Icon } from './Icon.astro';
 export { default as Heading } from './Heading.astro';
 export { default as Text } from './Text.astro';

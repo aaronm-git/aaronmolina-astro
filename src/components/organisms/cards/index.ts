3650ae79bd543edf2ed763733ca0107036cf57de
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2026-07-03 18:04:08 -0400

    chore: remove components and template superseded by Industrial Grotesk redesign

--- src/components/organisms/cards/index.ts
+++ src/components/organisms/cards/index.ts
@@ -2,5 +2,3 @@
 export { default as BlogPostCard } from './BlogPostCard.astro';
 export { default as ProjectCard } from './ProjectCard.astro';
 export { default as RoleCard } from './RoleCard.astro';
-export { default as TestimonialCard } from './TestimonialCard.astro';
-export { default as ServiceCard } from './ServiceCard.astro';
